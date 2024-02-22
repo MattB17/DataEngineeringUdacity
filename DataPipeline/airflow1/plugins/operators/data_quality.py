da80c3fe1d1487062b535ec982f6defdf2c32a26
MattB17 <matt.buckley1755@gmail.com>
2024-02-21 20:28:18 -0500

    Fixing data quality operator

--- DataPipeline/airflow1/plugins/operators/data_quality.py
+++ DataPipeline/airflow1/plugins/operators/data_quality.py
@@ -1,174 +1,52 @@
-from datetime import datetime, timedelta
-import pendulum
-import os
-from airflow.decorators import dag
-from airflow.operators.dummy_operator import DummyOperator
-from final_project_operators.stage_redshift import StageToRedshiftOperator
-from final_project_operators.load_fact import LoadFactOperator
-from final_project_operators.load_dimension import LoadDimensionOperator
-from final_project_operators.data_quality import DataQualityOperator
-from udacity.common import final_project_sql_statements
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
 
+class DataQualityOperator(BaseOperator):
 
-default_args = {
-    'owner': 'udacity',
-    'start_date': pendulum.now(),
-    'depends_on_past': False,
-    'catchup': False,
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'email_on_retry': False
-}
+    ui_color = '#89DA59'
 
-S3_BUCKET = "mattbx17-udacity-airflow"
-REDSHIFT_CONN_ID = "redshift"
-AWS_CREDENTIALS_ID = "aws_credentials"
-AWS_REGION = "us-east-1"
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 non_null_cols=[],
+                 *args, **kwargs):
 
-@dag(
-    default_args=default_args,
-    description='Load and transform data in Redshift with Airflow',
-    schedule_interval='0 * * * *'
-)
-def final_project():
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.non_null_cols = non_null_cols
 
-    start_operator = DummyOperator(task_id='Begin_execution')
+    def execute(self, context):
+        self.log.info("Starting Data Quality check for {}".format(self.table))
 
-    stage_events_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_events',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        aws_credentials_id=AWS_CREDENTIALS_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.staging_events_table_create,
-        destination_table="staging_events",
-        s3_bucket=S3_BUCKET,
-        s3_key="log-data/",
-        json_format="s3://{}/log_json_path.json".format(S3_BUCKET),
-        region=AWS_REGION
-    )
+        redshift = PostgresHook(self.redshift_conn_id)
+        self.validateHasRows(context, redshift)
+        self.validateNonNullCols(context, redshift)
 
-    stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_songs',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        aws_credentials_id=AWS_CREDENTIALS_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.staging_songs_table_create,
-        destination_table="staging_songs",
-        s3_bucket=S3_BUCKET,
-        s3_key="song-data/",
-        json_format="auto",
-        region=AWS_REGION
-    )
+    def validateHasRows(self, context, redshift_hook):
+        result = redshift_hook.get_records("SELECT COUNT(*) FROM {}".format(self.table))
+        if len(result) < 1 or len(result[0]) < 1:
+            raise ValueError("Has Rows check failed. {} returned no results".format(self.table))
+        record_count = result[0][0]
+        if record_count < 1:
+            raise ValueError("Has Rows check failed. {} contained 0 rows")
+        self.log.info("Has rows check on {0} passed with {1} records".format(self.table, record_count))
 
-    load_songplays_table = LoadFactOperator(
-        task_id='Load_songplays_fact_table',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.songplay_table_create,
-        destination_table="public.songplays",
-        table_select_statement=final_project_sql_statements.SqlQueries.songplay_table_insert,
-        execute_drop=True
-    )
-
-    load_user_dimension_table = LoadDimensionOperator(
-        task_id='Load_user_dim_table',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.user_table_create,
-        destination_table="public.user",
-        table_select_statement=final_project_sql_statements.SqlQueries.user_table_insert,
-        execute_drop=True
-    )
-
-    load_song_dimension_table = LoadDimensionOperator(
-        task_id='Load_song_dim_table',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.song_table_create,
-        destination_table="public.song",
-        table_select_statement=final_project_sql_statements.SqlQueries.song_table_insert,
-        execute_drop=True
-    )
-
-    load_artist_dimension_table = LoadDimensionOperator(
-        task_id='Load_artist_dim_table',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.artist_table_create,
-        destination_table="public.artist",
-        table_select_statement=final_project_sql_statements.SqlQueries.artist_table_insert,
-        execute_drop=True
-    )
-
-    load_time_dimension_table = LoadDimensionOperator(
-        task_id='Load_time_dim_table',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        create_table_statement=final_project_sql_statements.SqlQueries.time_table_create,
-        destination_table="public.time",
-        table_select_statement=final_project_sql_statements.SqlQueries.time_table_insert,
-        execute_drop=True
-    )
-
-    songplays_quality_check = DataQualityOperator(
-        task_id='songplays_quality_check',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        table="public.songplays",
-        non_null_cols=["songplay_id", "start_time", "user_id", "song_id", "artist_id", "session_id"]
-    )
-
-    user_quality_check = DataQualityOperator(
-        task_id='user_quality_check',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        table="public.user",
-        non_null_cols=["user_id"]
-    )
-
-    song_quality_check = DataQualityOperator(
-        task_id='song_quality_check',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        table="public.song",
-        non_null_cols=["song_id", "artist_id", "year", "duration"]
-    )
-
-    artist_quality_check = DataQualityOperator(
-        task_id='artist_quality_check',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        table="public.artist",
-        non_null_cols=["artist_id"]
-    )
-
-    time_quality_check = DataQualityOperator(
-        task_id='time_quality_check',
-        redshift_conn_id=REDSHIFT_CONN_ID,
-        table="public.time",
-        non_null_cols=["start_time"]
-    )
-
-    end_operator = DummyOperator(task_id="End_execution")
-
-    # First task is to upload data to staging tables, which can
-    # be done in parallel.
-    start_operator >> stage_events_to_redshift
-    start_operator >> stage_songs_to_redshift
-
-    # Next we build the fact table from the data in staging, so those
-    # tasks must be finished first.
-    stage_events_to_redshift >> load_songplays_table
-    stage_songs_to_redshift >> load_songplays_table
-
-    # We can now build the dimension tables which depend on the fact table.
-    load_songplays_table >> load_user_dimension_table
-    load_songplays_table >> load_song_dimension_table
-    load_songplays_table >> load_artist_dimension_table
-    load_songplays_table >> load_time_dimension_table
-    # We can also start quality checks on the songplays tables
-    load_songplays_table >> songplays_quality_check
-
-    # Once all the tables can be loaded we can perform data quality checks.
-    load_user_dimension_table >> user_quality_check
-    load_song_dimension_table >> song_quality_check
-    load_artist_dimension_table >> artist_quality_check
-    load_time_dimension_table >> time_quality_check
-
-    # After quality checks complete, we are finished.
-    songplays_quality_check >> end_operator
-    user_quality_check >> end_operator
-    song_quality_check >> end_operator
-    artist_quality_check >> end_operator
-    time_quality_check >> end_operator
-
-final_project_dag = final_project()
+    def validateNonNullCols(self, context, redshift_hook):
+        for col in self.non_null_cols:
+            result = redshift_hook.get_records("""
+              SELECT COUNT(*)
+              FROM {table}
+              WHERE {col} IS NULL
+            """.format(table=self.table, col=col))
+            if len(result) < 1 or len(result[0]) < 1:
+                raise ValueError("Non Null check failed. {} returned no results".format(self.table))
+            record_count = result[0][0]
+            if record_count > 0:
+                raise ValueError(
+                    "Non Null check failed. {table} had {records} rows with null {col}".format(table=self.table,
+                                                                                               records=record_count,
+                                                                                               col=col))
+            self.log.info("Non Null check passed for {col} on {table}".format(col=col, table=self.table))
